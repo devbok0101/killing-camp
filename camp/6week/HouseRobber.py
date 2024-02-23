07e71e966f6e997ec2c3a8c4ad2be610a671fdd9
devbok0209 <devbok0209@bodycodi.com>
2024-02-24 02:20:44 +0900

    [ADD] HouseRobber 문제 추가, 점화식이 중요함

---
+++ camp/6week/HouseRobber.py
@@ -0,0 +1,19 @@
+
+from typing import List
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = {}
+        def dp(n):
+            if n == 0:
+                return nums[0]
+            if n == 1:
+                return max(nums[0], nums[1])
+            if n not in memo:
+                memo[n] = max(dp(n - 1), dp(n - 2) + nums[n])
+            return memo[n]
+        return dp(len(nums) - 1)
+
+
+
+print(Solution.rob(Solution, nums=[2,7,9,3,1]))
